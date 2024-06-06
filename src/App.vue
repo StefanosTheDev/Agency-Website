6095809cc47a7c9b806e96b7c411ab0d39fafc37
Stefanos Sophocleous <stefanossophocleous@Stefanoss-MacBook-Pro.local>
2024-06-05 19:58:27 -0700

    Pushing some responsive changes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <template>
   <div class="main-wrapper">
     <header class="header-container">
-      <navbar class="navbar">
-        <img src="./assets/logo.svg" alt="ClearStack AI" class="logo" />
-        <div class="nav-links">
-          <a href="#">Services</a>
-          <a href="#">Our Team</a>
-          <a href="#">Contact</a>
-        </div>
-        <button class="btn-tertiary">Our Services</button>
-      </navbar>
+      <NavbarComponent />
       <HeaderComponent />
     </header>
     
@@ -29,6 +21,7 @@ import ServicesComponent from './components/Services.vue';
 import TeamComponent from './components/Team.vue';
 import ContactComponent from './components/Contact.vue';
 import FooterComponent from './components/Footer.vue';
+import NavbarComponent from './components/Navbar.vue';
 
 export default defineComponent({
   name: 'App',
@@ -37,7 +30,8 @@ export default defineComponent({
     ServicesComponent,
     TeamComponent,
     ContactComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   },
 });
 </script>
@@ -46,52 +40,6 @@ export default defineComponent({
 @font-face {
   font-family: "Jura";
   src: url("./assets/fonts/Jura.ttf");
-}
-
-.navbar {
-  display: flex;
-  width: 100%;
-  height: 140px;
-  box-sizing: border-box;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #F5F9FF;
-  padding: 0 60px;
-}
-
-.nav-links {
-  display: flex;
-  gap: 64px;
-  padding: 24px;
-  background-color: white;
-  border-radius: 98px;
-}
-
-.nav-links a {
-  color: #0075FF;
-  font-weight: normal;
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  font-size: 16px;
-  text-decoration: none;
-}
-
-.logo {
-  height: 60px;
-}
-
-.header-container {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100vh;
-}
-
-.input {
-  border: none;
-  border-bottom: 1px solid #9C9C9C;
-  background-color: transparent;
-  color: #001329;
-  padding: 8px;
 }
 
 body,
@@ -114,33 +62,11 @@ main {
   flex-direction: column;
 }
 
-.btn-primary {
-  background-color: #0075ff;
-  color: #fff;
-  font: 500 16px Segoe UI, sans-serif;
-  border-radius: 66px;
-  padding: 12px 32px;
-  border: none;
-  z-index: 2;
-  cursor: pointer;
-}
-
-.btn-primary:hover {
-  background-color: #0055cc;
-}
-
-.btn-secondary {
-  color: #0075ff;
-  background-color: #F5F9FF;
-  font: 500 16px Segoe UI, sans-serif;
-  border-radius: 66px;
-  padding: 12px 32px;
-  border: none;
-  cursor: pointer;
-}
-
-.btn-secondary:hover {
-  background-color: #B2D6FF;
+.header-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
 }
 
 .btn-tertiary {
@@ -182,7 +108,7 @@ h4 {
   font-weight: 700;
 }
 
-P {
+p {
   color: #001329;
   font-family: "Segoe UI", sans-serif;
   font-size: 20px;
--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,38 +1,45 @@
 <template>
-    <div class="services-container">
-        <section class="services-wrapper" ref="servicesSection">
-        <div class="services-words">
-            <h2>Our <span class="highlight">Services</span></h2>
-            <h3>We Provide Many Awesome Services</h3>
-            <p>Small introduction paragraph here about the company’s services and what we provide people with. Small introduction paragraph here about the company’s services and what we provide people with. Small introduction paragraph here about the company’s services and what we provide people with. Small introduction paragraph here about the company’s services and what we provide people with.</p>
-            <div class="spacing"></div>
-            <button class="btn-primary">Schedule a Call</button>        
-        </div>
-        <div class="services-grid">
-          <article class="service-card">
-            <div class="service-icon">
-              <img src="../assets/icons/consulting.svg" />
-            </div>
-            <h4>Consulting</h4>
-            <p>This is an overview of what kinds of consulting we do and what that even means. Here is more description.</p>
-          </article>
-          <article class="service-card">
-            <div class="service-icon">
-              <img src="../assets/icons/optimization.svg" />
-            </div>
-            <h4>Automated Process Optimization</h4>
-            <p>This is an overview of what kinds of processes we optimize and what that even means. Here is more description.</p>
-          </article>
-          <article class="service-card">
-            <div class="service-icon">
-              <img src="../assets/icons/chatbot.svg" />
-            </div>
-            <h4>ChatBot Development</h4>
-            <p>This is an overview of what kinds of chatbot dev we do and what that even means. Here is more description.</p>
-          </article>
-        </div>
-      </section>
-    </div>
+  <div class="services-container">
+    <section class="services-wrapper" ref="servicesSection">
+      <div class="services-words">
+        <h2>Our <span class="highlight">Services</span></h2>
+        <h3>We Provide Many Awesome Services</h3>
+        <p>Small introduction paragraph here about the company’s services and what we provide people with. Small introduction paragraph here about the company’s services and what we provide people with. Small introduction paragraph here about the company’s services and what we provide people with. Small introduction paragraph here about the company’s services and what we provide people with.</p>
+        <div class="spacing"></div>
+        <button class="btn-primary">Schedule a Call</button>        
+      </div>
+      <div class="services-grid">
+        <article class="service-card">
+          <div class="service-icon">
+            <img src="../assets/icons/consulting.svg" />
+          </div>
+          <h4>Consulting</h4>
+          <p>This is an overview of what kinds of consulting we do and what that even means. Here is more description.</p>
+        </article>
+        <article class="service-card">
+          <div class="service-icon">
+            <img src="../assets/icons/optimization.svg" />
+          </div>
+          <h4>Automated Process Optimization</h4>
+          <p>This is an overview of what kinds of processes we optimize and what that even means. Here is more description.</p>
+        </article>
+        <article class="service-card">
+          <div class="service-icon">
+            <img src="../assets/icons/chatbot.svg" />
+          </div>
+          <h4>ChatBot Development</h4>
+          <p>This is an overview of what kinds of chatbot dev we do and what that even means. Here is more description.</p>
+        </article>
+        <article class="service-card">
+          <div class="service-icon">
+            <img src="../assets/icons/chatbot.svg" />
+          </div>
+          <h4>ChatBot Development</h4>
+          <p>This is an overview of what kinds of chatbot dev we do and what that even means. Here is more description.</p>
+        </article>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -43,59 +50,100 @@ export default {
 
 <style scoped>
 
-    .spacing {
-        height: 32px;
-    }
+.spacing {
+  height: 32px;
+}
 
-    .services-container {
-        background-color: #F5F9FF;
-        width: 100%;
-        padding: 60px;
-        box-sizing: border-box;
-    }
-    .services-wrapper {
-        display: flex;
-        justify-content: space-between;
-        padding: 64px;
-        background-color: white;
-        border-radius: 32px;
-        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-    }
+.services-container {
+  background-color: #F5F9FF;
+  width: 100%;
+  padding: 60px;
+  box-sizing: border-box;
+}
 
-    .services-words{
-        width: 30%;
-        display: flex;
-        flex-direction: column;
-    }
+.services-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  padding: 64px;
+  background-color: white;
+  border-radius: 32px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
 
-    .services-grid{
-        width: 60%;
-        display: flex;
-        justify-content: space-between;
-        row-gap: 32px;
-        flex-wrap: wrap;
-    }
+.services-words {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
 
-    .service-card{
-        height: 340px;
-        display: flex;
-        flex-direction: column;
-        padding: 32px;
-        justify-content: space-between;
-        width: 35%;
-        align-items: center;
-        background-color: #F5F9FF;
-        border-radius: 16px;
-        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-    }
+.services-grid {
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+}
 
-    .service-icon{
-        width: 64px;
-        height: 64px;
-        border-radius: 50%;
-        background-color: #0075ff;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+.service-card {
+  display: flex;
+  flex-direction: column;
+  padding: 32px;
+  align-items: center;
+  background-color: #F5F9FF;
+  border-radius: 16px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+
+.service-icon {
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  background-color: #0075ff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@media (min-width: 768px) {
+  .services-wrapper {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  .services-words {
+    width: 30%;
+  }
+  .btn-primary {
+  background-color: #0075ff;
+  color: #fff;
+  font: 500 16px Segoe UI, sans-serif;
+  border-radius: 66px;
+  padding: 12px 32px;
+  border: none;
+  z-index: 2;
+  cursor: pointer;
+  text-align: center;
+}
+
+.btn-primary:hover {
+  background-color: #0055cc;
+}
+
+  .services-grid {
+    width: 60%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  .service-card {
+    width: 48%;
+  }
+}
+
+@media (min-width: 1024px) {
+  .service-card {
+    width: 32%;
+  }
+}
 </style>
