2467072a02e73427eb4734e099e783d2ebdfbcee
Jugooch <justicegooch@hotmail.com>
2024-06-07 14:03:10 -0700

    Updated Service-card styling

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -97,7 +97,7 @@ export default {
   background-color: #F5F9FF;
   border-radius: 16px;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  width: 32%;
+  width: 45%;
   box-sizing: border-box;
 }
 
